468586fe60f410e723a5253a27f4c5bd562312ed
igner <ignerowicz.adam@gmail.com>
2020-11-12 23:11:17 +0100

    Ad. 2) Added transfer lock below 2 accounts

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -41,14 +41,12 @@ namespace Bank
                         break;
                     case 3:
                         OutgoingTransfer();
-                        Console.WriteLine("Make an outgoing transfer");           //funkcja
                         break;
                     case 4:
                         PrintAccounts();
                         break;
                     case 5:
                         CheckTheHistoryOfTransfers();
-                                                                         //funkcja
                         break;
                     case 6:
                         QuitTheProgram(userChoice, exit=true);
@@ -60,11 +58,9 @@ namespace Bank
             } while (!exit);
         }
 
-        
-
         private void QuitTheProgram(int userChoice, bool exit)
         {
-            if (userChoice == 6)                                        //ew. funkcja
+            if (userChoice == 6)
             {
                 exit = true;
             }
@@ -75,15 +71,16 @@ namespace Bank
             List<Transfer> history = _transfersHistory.GetAllTransfers();
             foreach (Transfer transfers in history)
             {                                                                           
-                string text = @$"   Date of the transfer:  {transfers.DateOfTheTransfer}
-                                    GUID source account:   {transfers.SourceAccount}
-                                    GUID target account:   {transfers.TargetAccount}
-                                    Transfer Title:        {transfers.TransferTitle}  
-                                    Transfers Amount:      {transfers.TransferAmount}
-                                    Typ of transfer:       {transfers.TypOfTransfer}";
+                string text = @$"Date of the transfer:  {transfers.DateOfTheTransfer}
+                                 GUID source account:   {transfers.SourceAccount}
+                                 GUID target account:   {transfers.TargetAccount}
+                                 Transfer Title:        {transfers.TransferTitle}  
+                                 Transfers Amount:      {transfers.TransferAmount}
+                                 Typ of transfer:       {transfers.TypOfTransfer}";
                 Console.WriteLine(text);
                 Console.WriteLine();
             }
+
             if (history.Count == 0)
             {
                 Console.WriteLine("No transfers has been sent");
@@ -99,8 +96,7 @@ namespace Bank
             // IfIsNullOrWhiteSpace(a);
             Guid targetAccount = GetGuidFromUser("The number of GUID");
 
-            double amount;
-            amount = GetDoubleFromUser("Transfer amount");
+            double amount = GetDoubleFromUser("Transfer amount");
 
             List<Account> guidNumber = _finances.GetAllAccounts();
             Guid sourceAccount = default(Guid);
@@ -113,11 +109,11 @@ namespace Bank
                 }
             }
             
-            if (sourceAccount==default(Guid))
-            {
-                Console.WriteLine("No field found");
-                return;
-            }
+            //if (sourceAccount==default(Guid))
+            //{
+            //    Console.WriteLine("No field found");
+            //    return;
+            //}
             
             Transfer newTransferOut = new Transfer()
             {
@@ -130,11 +126,23 @@ namespace Bank
             };
             Console.WriteLine($"Date of the transfer: {newTransferOut.DateOfTheTransfer}");
 
-            //MakeADomesticTransfer(a, b, amount);
+            MakeAnOutgoingTransfer(c, amount);
 
             _transfersHistory.AddTransfer(newTransferOut);
+        }
 
-
+        private void MakeAnOutgoingTransfer(string account, double amount)
+        {
+            List<Account> outgoingTransfer = _finances.GetAllAccounts();
+            for (int i = 0; i < outgoingTransfer.Count; i++)
+            {
+                Account k = outgoingTransfer[i];
+                if (k.AccountName == account)
+                {
+                    k.AccountBalance = k.AccountBalance - amount;
+                    Console.WriteLine($"Account: \"{k.AccountName}\" - Balance: {k.AccountBalance}$");
+                }
+            }
         }
 
         private Guid GetGuidFromUser(string message)
@@ -149,57 +157,51 @@ namespace Bank
 
         private void DomesticTransfer()
         {
+            List<Account> guidNumber = _finances.GetAllAccounts();
+            if (guidNumber.Count < 2)
+            {
+                Console.WriteLine("It is not possible to make an enternal transfer - minimum 2 accounts needed");
+                return;
+            }
+
             PrintAccounts();
             Console.WriteLine();
 
             
+
             string a = GetTextFromUser("The account from which the funds will be withdrawn - enter your account name");
-            // IfIsNullOrWhiteSpace(a);
+            
+            for (int i = 0; i < guidNumber.Count; i++)
+            {
+                if (a != guidNumber[i].AccountName)
+                {
+                    Console.WriteLine("There is no such account");
+                    return;
+                }
+            }
             
             string b = GetTextFromUser("The account to which the funds will be transferred - enter your account name");
-            // IfIsNullOrWhiteSpace(b);
+            for (int i = 0; i < guidNumber.Count; i++)
+            {
+                if (b != guidNumber[i].AccountName)
+                {
+                    Console.WriteLine("There is no such account");
+                    return;
+                }
+            }
 
-            //Account debitAccount = GetAccountByName(a);
-            //Account creditAccount = GetAccountByName(b);
-            double amount;
-            //bool correctAmount;
+            double amount = GetDoubleFromUser("Transfer amount");
+            for (int i = 0; i < guidNumber.Count; i++)
+            {
+                if (guidNumber[i].AccountName == a)
+                {
+                    while (amount < 0 || amount > guidNumber[i].AccountBalance)
+                    {
+                        Console.WriteLine("Wrong amount - try again...");
+                    }
+                }
+            }
 
-            //do
-            //{
-               amount = GetDoubleFromUser("Transfer amount");
-
-
-
-            //                                //for (int i = 0; i < accounts.Count; i++)
-            //                                //{
-            //                                //    if (accounts[i].AccountName == a)
-            //                                //    {
-            //                                //        debitAccount = accounts[i];
-            //                                //    };
-            //                                //    if (accounts[i].AccountName == b)
-            //                                //    {
-            //                                //        creditAccount = accounts[i];
-            //                                //    };                    
-            //                                //}
-
-            //    correctAmount = (amount > 0 && amount <= debitAccount.AccountBalance);
-            //    if (!correctAmount)
-            //    {
-            //        Console.WriteLine("zła kwota");
-            //    }
-            //} while (!correctAmount);
-
-
-
-
-
-            //    while (amount < 0 || amount > newAccount.AccountBalance)
-            //    {
-            //        Console.WriteLine("Wrong amount - try again...");
-            //        amount = GetDoubleFromUser("Transfer amount");
-            //    }
-            //}
-            List<Account> guidNumber = _finances.GetAllAccounts();
             Guid sourceAccount = default(Guid);
             Guid targetAccount = default(Guid);
 
@@ -208,57 +210,98 @@ namespace Bank
                 if (guidNumber[i].AccountName == a)
                 {
                     sourceAccount = guidNumber[i].AccountNumber;
-
                 }
+
                 if (guidNumber[i].AccountName == b)
                 {
                     targetAccount = guidNumber[i].AccountNumber;
                 }
             }
 
-            if (sourceAccount == default(Guid))
-            {
-                Console.WriteLine("No field found");
-                return;
-            }
+            //if (sourceAccount == default(Guid))
+            //{
+            //    Console.WriteLine("No field found");
+            //    return;
+            //}
 
-            if (targetAccount == default(Guid))
+            //if (targetAccount == default(Guid))
+            //{
+            //    Console.WriteLine("No field found");
+            //    return;
+            //}
+            string title = GetTextFromUser("Transfer title");
+            while (string.IsNullOrWhiteSpace(title))
             {
-                Console.WriteLine("No field found");
-                return;
+                Console.WriteLine("Incorrect title - try again...");
+                title = GetTextFromUser("Transfer title");
             }
 
             Transfer newTransfer = new Transfer()
             {
-                TransferTitle = GetTextFromUser("Transfer title"),
+                TransferTitle = title,
                 TransferAmount = amount,
                 DateOfTheTransfer = DateTime.Now,
                 TypOfTransfer = "Internal transfer",
                 SourceAccount = sourceAccount,
                 TargetAccount = targetAccount,
-
             };
             Console.WriteLine($"Date of the transfer: {newTransfer.DateOfTheTransfer}");
 
             MakeADomesticTransfer(a, b, amount);
 
             _transfersHistory.AddTransfer(newTransfer);
-
         }
 
-                        //private Account GetAccountByName(string accountName)
-                        //{
-                        //    List<Account> accounts = _finances.GetAllAccounts();
-                        //    Account accountToReturn = null;
-                        //    foreach (Account account in accounts)
-                        //    {
-                        //        if (account.AccountName == accountName)
-                        //        {
-                        //            accountToReturn = account;
-                        //        }               
-                        //    };
-                        //    return accountToReturn;
-                        //}
+        //Account debitAccount = GetAccountByName(a);
+        //Account creditAccount = GetAccountByName(b);
+
+        //bool correctAmount;
+
+        //do
+        //{
+        //                                //for (int i = 0; i < accounts.Count; i++)
+        //                                //{
+        //                                //    if (accounts[i].AccountName == a)
+        //                                //    {
+        //                                //        debitAccount = accounts[i];
+        //                                //    };
+        //                                //    if (accounts[i].AccountName == b)
+        //                                //    {
+        //                                //        creditAccount = accounts[i];
+        //                                //    };                    
+        //                                //}
+
+        //    correctAmount = (amount > 0 && amount <= debitAccount.AccountBalance);
+        //    if (!correctAmount)
+        //    {
+        //        Console.WriteLine("zła kwota");
+        //    }
+        //} while (!correctAmount);
+
+
+
+
+
+        //    while (amount < 0 || amount > newAccount.AccountBalance)
+        //    {
+        //        Console.WriteLine("Wrong amount - try again...");
+        //        amount = GetDoubleFromUser("Transfer amount");
+        //    }
+        //}
+
+        //private Account GetAccountByName(string accountName)
+        //{
+        //    List<Account> accounts = _finances.GetAllAccounts();
+        //    Account accountToReturn = null;
+        //    foreach (Account account in accounts)
+        //    {
+        //        if (account.AccountName == accountName)
+        //        {
+        //            accountToReturn = account;
+        //        }               
+        //    };
+        //    return accountToReturn;
+        //}
 
         //private void IfIsNullOrWhiteSpace(string instruction)
         //{
@@ -269,7 +312,7 @@ namespace Bank
         //    }
 
         //}
-        
+
         private void MakeADomesticTransfer(string account1, string account2, double amount)
         {
             List <Account> domesticTransfer = _finances.GetAllAccounts();
@@ -281,14 +324,13 @@ namespace Bank
                         k.AccountBalance = k.AccountBalance - amount;
                         Console.WriteLine($"Account: \"{k.AccountName}\" - Balance: {k.AccountBalance}$");
                     }
+                    
                     if (k.AccountName == account2)
                     {
                         k.AccountBalance = k.AccountBalance + amount;
                         Console.WriteLine($"Account: \"{k.AccountName}\" - Balance: {k.AccountBalance}$");
                     }
             }
-            
-            
         }
 
         private double GetDoubleFromUser(string message)
@@ -310,50 +352,39 @@ namespace Bank
                 string text = $"Account: \"{account.AccountName}\" - Balance: {account.AccountBalance}$ - Account Number: {account.AccountNumber}";
                 Console.WriteLine(text);
             }
+            
             if (yourAccounts.Count == 0)
             {
                 Console.WriteLine("No accounts has been created");
             } 
-            
-
-            
-        }
-
-        private int GetIntFromUser(string message)
-        {
-           int number;
-           while (!int.TryParse(GetTextFromUser(message), out number))
-           {
-               Console.WriteLine("Incorrect option - try again...");
-           }
-           return number;
-           
         }
 
         private void CreateAccount()
         {
             Console.WriteLine("Create Account:");
-
+            
             string accountName = GetTextFromUser("Provide account name");
             while (string.IsNullOrWhiteSpace(accountName))
             {
                 Console.WriteLine("Incorrect name - try again...");
                 accountName = GetTextFromUser("Provide account name");
             }
+
             List<Account> sprawdzenie = _finances.GetAllAccounts();
             for (int i = 0; i < sprawdzenie.Count; i++)
             {
-               while (accountName == sprawdzenie[i].AccountName )                                                                     //IfIsNullOrWhiteSpace(accountName);
-                {
-                    Console.WriteLine("The name exist - try again...");
-                }
+               while (accountName == sprawdzenie[i].AccountName )
+               {
+                   Console.WriteLine("The name exist - try again...");
+                   return;
+               }
             }
 
             Account newAccount = new Account()
             {
-                AccountName = accountName,      // nazwa konta: 1. nie pusta; 2. nie taka sama jak juz jest!! 
+                AccountName = accountName,
                 AccountNumber = GenerateGuidToUser(),
-                AccountBalance = 1000,                                      // wartosc konta poczatkowa!!!!!!!!!!!!!
+                AccountBalance = 1000,
             };
             
             Console.WriteLine($"The opening balance of the account: {newAccount.AccountBalance}$");
@@ -369,11 +400,20 @@ namespace Bank
             return g;
         }
 
+        private int GetIntFromUser(string message)
+        {
+            int number;
+            while (!int.TryParse(GetTextFromUser(message), out number))
+            {
+                Console.WriteLine("Incorrect option - try again...");
+            }
+            return number;
+        }
+
         private string GetTextFromUser(string message)
         {
             Console.Write($"{message}: ");
             return Console.ReadLine();
-
         }
     }
 }
